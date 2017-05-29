d7d5ef7e8b4a17e5c64f17570d13d7794766d272
soul6reaver <marko.d@scopicsoftware.com>
2017-05-29 14:36:58 +0200

    some login changes

--- app/Modules/AdminPart/routes/web.php
+++ app/Modules/AdminPart/routes/web.php
@@ -3,40 +3,18 @@
 Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
 
     Route::resource('AdminPart', 'AdminPartController');
-    
-});	
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
+    Route::resource('login', 'LoginController');
 
+    Route::get('/doLogin', 'LoginController@doLogin');
+    Route::post('/doLogin', 'LoginController@doLogin');
+
+    Route::get('/doLogout', 'LoginController@doLogout');
+    
     Route::resource('UserAccount', 'UserAccountController');
-    
-});	
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('Feedback', 'FeedbackController');
-    
-});	
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('PracticeAccount', 'PracticeAccountController');
-    
-});
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('BillingAndPayment', 'BillingAndPaymentController');
-    
-});	
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('AddSubscription', 'AddSubscriptionController');
-    
-});
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('MyKnowledgeBox', 'MyKnowledgeBoxController');
-    
-});	
-Route::group(array('module' => 'AdminPart', 'middleware' => ['web'], 'namespace' => 'App\Modules\AdminPart\Controllers'), function() {
-
     Route::resource('ReportProblem', 'ReportProblemController');
-    
 });	
