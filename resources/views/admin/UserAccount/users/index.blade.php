4b121aa158e2f2e168b8c66c3e31ece9db716bc0
kuna <soul6reaver@gmail.com>
2017-06-19 22:29:07 +0200

    users page

---
+++ resources/views/admin/UserAccount/users/index.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.default-admin-template')
+@section('AditionalHead')
+
+@stop
+
+@section('MainContent')
+	@include('admin.UserAccount.users.title')
+  
+  <div class="toolbar">
+    <input type="hidden" id="orderby" name="orderby" value="{{ $orderby }}">
+    <input type="hidden" id="sortby" name="sortby" value="{{ $sortby }}">
+  </div>
+
+  <div id="table-section" class="table-section">
+    {!! view('admin.UserAccount.users.table', [
+        'users' => $users,
+        'columns' => $columns,
+        'pagination' => $pagination,
+    ]) !!}
+  </div>
+@stop
+
+@section('AditionalFoot')
+		<script src="{{ URL::asset('/js/Admin/users.js') }}"></script>
+		<script type="text/javascript">
+        Users.init();
+    </script>
+@stop
