6cae746a7bdb4ebac07a35f71a7c554e2d8b8301
Dusan Stojanovic <dushan887@gmail.com>
2017-07-13 14:54:14 +0200

    Pricing page public part

---
+++ resources/views/public/PricingPlan/content.blade.php
@@ -0,0 +1,73 @@
+<section class="im-center small-padding" id="pricing-plan-menu">
+	<div class="container">
+	  <div class="row">
+	    <div class="col-md-4">
+	      <div class="pricing-table im-blue">
+
+	        <div class="pricing-table-header im-center">
+	          <div class="badge-cont bg-blue"><img src="http://imedical.local/img/pricing-3.svg" alt=""></div>
+	          <h3>BASIC<br> PLAN</h3>
+	        </div>
+
+	        <div class="pricing-table-body im-center">
+	          <a class="btn im-btn blue-btn" href="#">View Pricing</a>
+	          <h5>Lorem ipsum dolor sit amet</h5>
+	          <p class="im-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu condimentum arcu. Sed urna arcu, faucibus ullamcorper nisl eu, lobortis egestas nunc. Duis eget dolor erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+	        </div>
+
+	        <div class="pricing-table-footer im-center">
+	          <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+	        </div>
+
+	      </div>
+
+	    </div><!--/col--> 
+
+
+	     <div class="col-md-4">
+	      <div class="pricing-table im-lblue">
+
+	        <div class="pricing-table-header im-center">
+	        <div class="badge-cont bg-lblue"><img src="http://imedical.local/img/pricing-2.svg" alt=""></div>
+	          <h3>BUSINESS<br> PLAN</h3>
+	        </div>
+
+	        <div class="pricing-table-body im-center">
+	          <a class="btn im-btn lblue-btn" href="#">View Pricing</a>
+	          <h5>Lorem ipsum dolor sit amet</h5>
+	          <p class="im-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu condimentum arcu. Sed urna arcu, faucibus ullamcorper nisl eu, lobortis egestas nunc. Duis eget dolor erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+	        </div>
+
+	        <div class="pricing-table-footer im-center">
+	          <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+	        </div>
+
+	      </div>
+
+	    </div><!--/col--> 
+
+
+	     <div class="col-md-4">
+	      <div class="pricing-table im-pink">
+
+	        <div class="pricing-table-header im-center">
+	          <div class="badge-cont bg-pink"><img src="http://imedical.local/img/pricing-1.svg" alt=""></div>
+	          <h3>PREMIUM<br> PLAN</h3>
+	        </div>
+
+	        <div class="pricing-table-body im-center">
+	          <a class="btn im-btn pink-btn" href="#">View Pricing</a>
+	          <h5>Lorem ipsum dolor sit amet</h5>
+	          <p class="im-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu condimentum arcu. Sed urna arcu, faucibus ullamcorper nisl eu, lobortis egestas nunc. Duis eget dolor erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+	        </div>
+
+	        <div class="pricing-table-footer im-center">
+	          <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+	        </div>
+
+	      </div>
+
+	    </div><!--/col-->   
+	  </div><!--/row-->
+	</div>
+</section>
