e9a15ea9a247bda81b74fd37cd9f82bc1931f8c7
kuna <soul6reaver@gmail.com>
2017-07-18 17:06:35 +0200

    forgot files

--- database/migrations/2017_07_05_121329_BlogCategoriesTable.php
+++ database/migrations/2017_07_05_121329_BlogCategoriesTable.php
@@ -14,6 +14,7 @@ class BlogCategoriesTable extends Migration
     public function up()
     {
       Schema::create('blog_categories', function (Blueprint $table) {
+          $table->increments();
           $table->integer('blogs_id');
           $table->integer('categories_id');
           $table->timestamps();
