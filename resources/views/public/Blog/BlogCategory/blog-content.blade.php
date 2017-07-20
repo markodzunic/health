d6a42958571f7965af67a14dc4a6e9a527dc3c4f
kuna <soul6reaver@gmail.com>
2017-07-20 15:06:31 +0200

    some blog stuff

--- resources/views/public/Blog/BlogCategory/blog-content.blade.php
+++ resources/views/public/Blog/BlogCategory/blog-content.blade.php
@@ -1,70 +1,21 @@
 <div id="blog-grid">
 	<div class="row">
-
-		<div class="col-md-12 col-sm-12">
-			<article>
-				<div class="featured-image no-padding-top ">
-					<img src="http://placehold.it/1920x1080" alt="">
-				</div>
-				<div class="blog-expert-content">
-					<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">Lorem ipsum dolor</h4></a>
-					<p class="blog-expert">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
-				</div>
-			</article>
-		</div>
-
-		<div class="col-md-12 col-sm-12">
-			<article>
-				<div class="featured-image no-padding-top ">
-					<img src="http://placehold.it/1920x1080" alt="">
-				</div>
-				<div class="blog-expert-content">
-					<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">Lorem ipsum dolor</h4></a>
-					<p class="blog-expert">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
-				</div>
-			</article>
-		</div>
-
-		<div class="col-md-12 col-sm-12">
-			<article>
-				<div class="featured-image no-padding-top ">
-					<img src="http://placehold.it/1920x1080" alt="">
-				</div>
-				<div class="blog-expert-content">
-					<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">Lorem ipsum dolor</h4></a>
-					<p class="blog-expert">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
-				</div>
-			</article>
-		</div>
-
-		<div class="col-md-12 col-sm-12">
-			<article>
-				<div class="featured-image no-padding-top ">
-					<img src="http://placehold.it/1920x1080" alt="">
-				</div>
-				<div class="blog-expert-content">
-					<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">Lorem ipsum dolor</h4></a>
-					<p class="blog-expert">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
-				</div>
-			</article>
-		</div>
-
-		<div class="col-md-12 col-sm-12">
-			<article>
-				<div class="featured-image no-padding-top ">
-					<img src="http://placehold.it/1920x1080" alt="">
-				</div>
-				<div class="blog-expert-content">
-					<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">Lorem ipsum dolor</h4></a>
-					<p class="blog-expert">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
-				</div>
-			</article>
-		</div>
+		@if ($blogs)
+			@foreach ($blogs as $blog)
+					<div class="col-md-12 col-sm-12">
+						<article>
+							<div class="featured-image no-padding-top ">
+								<img src="{{ asset('/img/'.$blog->image) }}" alt="">
+							</div>
+							<div class="blog-expert-content">
+								<a href="{{ URL::to('/blog-single') }}"><h4 class="blog-title im-pink">{{ $blog->title?:'N/A' }}</h4></a>
+								<p class="blog-expert">{{ $blog->description?:'N/A' }}</p>
+								<a href="{{ URL::to('/blog-single') }}" class="btn full-btn im-btn lblue-btn">Read More</a>
+							</div>
+						</article>
+					</div>
+				@endforeach
+			@endif
 
 	</div>
 </div>
