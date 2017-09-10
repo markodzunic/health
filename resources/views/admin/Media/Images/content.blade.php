cea2cd9ad1c3469349eef916a303a4c3a8b4c1d9
Unknown <soul6reaver@gmail.com>
2017-09-10 16:38:38 +0200

    media images strana

--- resources/views/admin/Media/Images/content.blade.php
+++ resources/views/admin/Media/Images/content.blade.php
@@ -1,14 +1,45 @@
-<section class="small-padding">
+<section class="">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<a onclick="" class="btn im-btn empty-btn im-border-btn">Add New</a>
+			</div>
+		</div>
+	</div>
 	<div class="container-fluid">
 		<div id="gallery-grid">
 			<ul>
+
 				<li>
 					<div class="gallery-item">
-						<img src="http://via.placeholder.com/350x150">
-						
+						<img src="http://via.placeholder.com/150x150">
+
+						<div class="button-container">
+							<a href="#" onclick="" class="delete btn im-btn no-margin-bottom im-btn-small">
+							<i class="fa fa-trash" aria-hidden="true"></i>
+							<div class="im-btn-info">Delete Image</div></a>
+
+							<a href="#" onclick="" class="delete btn im-btn no-margin-bottom im-btn-small">
+							<i class="fa fa-search" aria-hidden="true"></i>
+							<div class="im-btn-info">View Image</div></a>
+						</div>
 					</div>
 				</li>
+
 			</ul>
 		</div>
 	</div>
+
+	<div id="image-preview">
+		<div class="image-preview-overlay"></div>
+		<div class="preview-container">
+
+		<a href="#" class="close2"><i class="fa fa-close im-white"></i></a>
+			<div class="container-i im-center">
+				<img src="http://via.placeholder.com/1200x800">
+			</div>
+		</div>
+	</div>
+
+
 </section>
