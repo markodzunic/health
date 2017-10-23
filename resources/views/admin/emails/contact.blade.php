c3486de28a702d116ef1eb2302d53f2381be8c7c
kuna <soul6reaver@gmail.com>
2017-10-23 14:39:34 +0200

    mails added

---
+++ resources/views/admin/emails/contact.blade.php
@@ -0,0 +1,17 @@
+<div class="">
+    <div class="">
+      Name: {{ $contact['first_name']. ' '. $contact['last_name'] }}
+    </div>
+    <div class="">
+      Position: {{ $contact['position']?:'N/A' }}
+    </div>
+    <div class="">
+      Practice Name: {{ $contact['practice_name']?:'N/A' }}
+    </div>
+    <div class="">
+      Phone: {{ $contact['phone']?:'N/A' }}
+    </div>
+    <div class="">
+      Message: {{ $contact['description']?:'N/A' }}
+    </div>
+</div>
--- resources/views/public/Contact/content.blade.php
+++ resources/views/public/Contact/content.blade.php
@@ -1,4 +1,4 @@
-<section>	
+<section>
 	<div class="small-padding">
 		<div class="container">
 			<div class="row">
@@ -10,8 +10,12 @@
 			</div>
 			<div class="row">
 				<div class="col-md-10 col-sm-12 col-md-offset-1">
+					@if(Session::has('alert-success'))
+						<div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('alert-success') !!}</em></div>
+					@endif
 					<p class="h4 im-pink">Please complete this form</p>
-					<form class="line-form">
+					<form class="line-form" method="POST" action="/contact/create">
+						{{ csrf_field() }}
 						<div class="row">
 							<div class="col-md-12">
 								<p><strong>Fields marked with an asterisk are required</strong></p>
@@ -21,45 +25,80 @@
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									First Name<span> *</span>
-									<input type="text">
+									<input name="first_name" type="text">
 								</label>
+								@if ($errors->has('first_name'))
+										<span class="help-block">
+												<strong>{{ $errors->first('first_name') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									Last Name<span> *</span>
-									<input type="text">
+									<input name="last_name" type="text">
 								</label>
+								@if ($errors->has('last_name'))
+										<span class="help-block">
+												<strong>{{ $errors->first('last_name') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									Position / Title<span> *</span>
-									<input type="text">
+									<input name="position" type="text">
 								</label>
+								@if ($errors->has('position'))
+										<span class="help-block">
+												<strong>{{ $errors->first('position') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									Practice Name<span> *</span>
-									<input type="text">
+									<input name="practice_name" type="text">
 								</label>
+								@if ($errors->has('practice'))
+										<span class="help-block">
+												<strong>{{ $errors->first('practice') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									Phone<span> *</span>
-									<input type="text">
+									<input name="phone"type="text">
 								</label>
+								@if ($errors->has('phone'))
+										<span class="help-block">
+												<strong>{{ $errors->first('phone') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-sm-6 col-xs-12">
 								<label>
 									Email<span> *</span>
-									<input type="text">
+									<input name="email" type="text">
 								</label>
+								@if ($errors->has('email'))
+										<span class="help-block">
+												<strong>{{ $errors->first('email') }}</strong>
+										</span>
+								@endif
 							</div>
 							<div class="col-xs-12">
 								<label>
 									Message<span> *</span>
-									<textarea></textarea>
+									<textarea name="description"></textarea>
 								</label>
-							</div>					
+								@if ($errors->has('description'))
+										<span class="help-block">
+												<strong>{{ $errors->first('description') }}</strong>
+										</span>
+								@endif
+							</div>
 							<div class="col-xs-12 im-center">
 								<div style="height: 30px"></div>
 								<button href="#" class="btn im-btn pink-btn">Send Message</button>
