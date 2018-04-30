9c55285e63e6cba80a56c9e6893ef0b4e2c7daaa
top-dev <dushan887@gmail.com>
2018-04-30 20:17:41 +0200

    Revert "stripe"
    This reverts commit f2e695580041feb3eb3a2623635115aafcf6dd09.

--- resources/views/admin/AddSubscription/PlanBasic/content.blade.php
+++ resources/views/admin/AddSubscription/PlanBasic/content.blade.php
@@ -28,7 +28,7 @@
 
         <form accept-charset="UTF-8" action="/payment" class="require-validation"
           data-cc-on-file="false"
-          data-stripe-publishable-key="pk_test_cQgFysLq5ZUV1z757YaQ3j2d"
+          data-stripe-publishable-key="sk_live_t0y0GpaiYZKBHKpFQL6NpI6g"
           id="payment-form" method="post">
           {{ csrf_field() }}
           <div class='form-row'>
